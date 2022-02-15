aa0fe37ccc54b1d644a78fd72849e01285e95462
Rinchen <rinchenn@gmail.com>
2022-02-15 09:19:43 -0500

    update models.py

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,6 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-
 SECRET_KEY = os.urandom(32)
 DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
 DB_USER = os.getenv('DB_USER', 'postgres')
@@ -23,8 +22,7 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
 
-
-def setup_db(app, database_path=database_path):
+def setup_db(app, database_path=DB_PATH):
     app.config["SQLALCHEMY_DATABASE_URI"] = DB_PATH
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SECRET_KEY"] = SECRET_KEY
